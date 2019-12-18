ff81eaa517fd899d505e1d7546cd0ef3a2a3443a
Owen Flaherty <o.flaherty@theeastbourneacademy.org>
2019-12-18 08:40:15 +0000

    Added Counting Sort

---
+++ Sort - Counting Sort/countingsort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+void doSort(char arr[], int range)
+{
+    char out[strlen(arr)];
+    int count[range + 1], i;
+    memset(count, 0, sizeof(count));
+    for (i = 0; arr[i]; i++)
+        count[arr[i]]++;
+    for (i = 1; i <= range; i++)
+        count[i] += count[i-1];
+    for (i = 0; arr[i]; i++)
+    {
+        out[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+    for (i = 0; arr[i]; i++)
+        arr[i] = out[i];
+}
+
+void printArray(char arr[], int szArr)
+{
+    int i;
+    for (i = 0; i < szArr; i++)
+        printf("%c ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    char arr[] = "owenflaherty";
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+    doSort(arr, 255);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
